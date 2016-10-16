67bedebc38d74fc59d5586869c15c196342bc6f0
Marcel Gohsen <marcel.gohsen@web.de>
2016-10-16 16:31:33 +0200

    Removed planet normalization and updated window size.

--- application/include/planet.hpp
+++ application/include/planet.hpp
@@ -4,22 +4,14 @@
 //Assign. 1
 struct Planet {
 
-	Planet(float siz, float speed, float dist):
-		size{siz},
+	Planet(float scal, float speed, float dist):
+		scale{scal},
 		rot_speed{speed},
-		distance_from_sun{dist}
-	{
-        scale = size / (695700 * 2); //size / (max radius (sun) * (1/scaling_max))
-        orig_distance = 6 * distance_from_sun / 4503443661; //distance / (max distance (neptune) * (1/max distance))
-    }
+		orig_distance{dist}
+	{}
 
-    //real
-	float size; //in km
+	float scale; //scaling factor
 	float rot_speed;
-	float distance_from_sun; //in km
-
-    //virtual
-    float scale; //scaling factor
     float orig_distance;
 };
 
