eb6dfb8ff8764e188a9bf20a999c5cdfdc494103
Marcel Gohsen <marcel.gohsen@web.de>
2016-10-20 11:11:50 +0200

    make moons rotate around planets

---
+++ build/CMakeFiles/framework.dir/DependInfo.cmake
@@ -0,0 +1,43 @@
+# The set of languages for which implicit dependencies are needed:
+set(CMAKE_DEPENDS_LANGUAGES
+  "CXX"
+  )
+# The set of files for implicit dependencies of each language:
+set(CMAKE_DEPENDS_CHECK_CXX
+  "/home/marcel/Documents/uni/Semester 5/stable/Computergrafik/external/tinyobjloader-aa07206/tiny_obj_loader.cc" "/home/marcel/Documents/uni/Semester 5/stable/Computergrafik/build/CMakeFiles/framework.dir/external/tinyobjloader-aa07206/tiny_obj_loader.cc.o"
+  "/home/marcel/Documents/uni/Semester 5/stable/Computergrafik/framework/source/application.cpp" "/home/marcel/Documents/uni/Semester 5/stable/Computergrafik/build/CMakeFiles/framework.dir/framework/source/application.cpp.o"
+  "/home/marcel/Documents/uni/Semester 5/stable/Computergrafik/framework/source/launcher.cpp" "/home/marcel/Documents/uni/Semester 5/stable/Computergrafik/build/CMakeFiles/framework.dir/framework/source/launcher.cpp.o"
+  "/home/marcel/Documents/uni/Semester 5/stable/Computergrafik/framework/source/model.cpp" "/home/marcel/Documents/uni/Semester 5/stable/Computergrafik/build/CMakeFiles/framework.dir/framework/source/model.cpp.o"
+  "/home/marcel/Documents/uni/Semester 5/stable/Computergrafik/framework/source/model_loader.cpp" "/home/marcel/Documents/uni/Semester 5/stable/Computergrafik/build/CMakeFiles/framework.dir/framework/source/model_loader.cpp.o"
+  "/home/marcel/Documents/uni/Semester 5/stable/Computergrafik/framework/source/shader_loader.cpp" "/home/marcel/Documents/uni/Semester 5/stable/Computergrafik/build/CMakeFiles/framework.dir/framework/source/shader_loader.cpp.o"
+  "/home/marcel/Documents/uni/Semester 5/stable/Computergrafik/framework/source/texture_loader.cpp" "/home/marcel/Documents/uni/Semester 5/stable/Computergrafik/build/CMakeFiles/framework.dir/framework/source/texture_loader.cpp.o"
+  "/home/marcel/Documents/uni/Semester 5/stable/Computergrafik/framework/source/utils.cpp" "/home/marcel/Documents/uni/Semester 5/stable/Computergrafik/build/CMakeFiles/framework.dir/framework/source/utils.cpp.o"
+  )
+set(CMAKE_CXX_COMPILER_ID "GNU")
+
+# Preprocessor definitions for this target.
+set(CMAKE_TARGET_DEFINITIONS_CXX
+  "GLBINDING_STATIC_DEFINE"
+  "SYSTEM_LINUX"
+  )
+
+# The include file search paths:
+set(CMAKE_CXX_TARGET_INCLUDE_PATH
+  "../external/glm-0.9.6.3"
+  "../external/stb_image-2.0.6"
+  "../external/tinyobjloader-aa07206"
+  "../external/glfw-3.1.1/include"
+  "../external/glbinding-2.1.1/source/glbinding/include"
+  "../framework/include"
+  "../application/include"
+  "external/glbinding-2.1.1/source/glbinding/include"
+  )
+
+# Targets to which this target links.
+set(CMAKE_TARGET_LINKED_INFO_FILES
+  "/home/marcel/Documents/uni/Semester 5/stable/Computergrafik/build/external/glbinding-2.1.1/source/glbinding/CMakeFiles/glbinding.dir/DependInfo.cmake"
+  "/home/marcel/Documents/uni/Semester 5/stable/Computergrafik/build/external/glfw-3.1.1/src/CMakeFiles/glfw.dir/DependInfo.cmake"
+  )
+
+# Fortran module output directory.
+set(CMAKE_Fortran_TARGET_MODULE_DIR "")
